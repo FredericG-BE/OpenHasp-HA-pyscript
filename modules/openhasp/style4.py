68c89764a718fe19f33f6ccaf3d242fd44a524cf
Frederic Goddeeris <FredericGoddeeris@hotmail.com>
2024-02-07 16:48:15 +0100

    Added Style4

---
+++ modules/openhasp/style4.py
@@ -0,0 +1,25 @@
+PAL_LIGHTGRAY = "#EBE4D1"
+PAL_BLUE = "#26577C"
+PAL_OCHER = "#E55604"
+PAL_LIGHTBLUE = "#647D87"
+
+style = (  {"page.gb_color":       PAL_LIGHTGRAY,
+            "text.color":          PAL_OCHER,
+            "text.align":          "center",
+            "text.on.color":       "Gold",
+            "text.off.color":      "Linen",
+            "btn.text_color":      "Linen",
+            "btn.bg_color":        PAL_LIGHTGRAY,
+            "btn.radius":          20,
+            "btn.align":           "center",
+            "btn.border_color":    PAL_LIGHTBLUE,
+            "btn.border_width":    2,
+            "btn.on.bg_color":     PAL_BLUE,
+            "btn.off.bg_color":    PAL_LIGHTGRAY,
+            "btn.unavailable.bg_color": "Red",
+            "nav.bg_color":        PAL_LIGHTGRAY,
+            "nav.active.bg_color": PAL_BLUE,
+            "line.color":          "Linen",
+            "line.width":          10,
+            "clock.color":         PAL_OCHER,
+            })
