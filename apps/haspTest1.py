fc8968a835f56c2d338b4e4d77828a7daf0b6df6
Frederic Goddeeris <FredericGoddeeris@hotmail.com>
2024-02-07 10:17:48 +0100

    test calling service

--- apps/haspTest1.py
+++ apps/haspTest1.py
@@ -17,7 +17,11 @@ oh.Page(design, 1)
 obj = oh.Button(design, 10, 10, 200, 100, "TestMe", 50)
 obj.actionOnPush(onTestMePushed)
 
-obj = oh.Label(design, 250, 33, 200, 100, "0", 50)
+obj = oh.Label(design, 250, 33, 200, 100, "NA", 50)
 obj.linkText("input_number.demo")
+
+obj = oh.Button(design, 10, 150, 400, 100, "Call Service", 50)
+obj.serviceOnPush("light", "toggle", entity_id="light.bureau_rail")
+
 
 manager.sendDesign()
