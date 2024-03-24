a102cf015bd32f03ddb8556b0c524d3d4d298a9d
Frederic Goddeeris <FredericGoddeeris@hotmail.com>
2024-03-24 19:00:15 +0100

    Add plane name to temp image files

--- modules/openhasp/imageHandling.py
+++ modules/openhasp/imageHandling.py
@@ -1,8 +1,10 @@
 from PIL import Image
 import requests
-import hashlib
+import time
 import struct
 import logging
+
+from homeassistant.helpers.network import get_url
 
 
 @pyscript_executor
@@ -15,14 +17,14 @@ def getSize(src):
     except Exception:
         return None
 
-    return im.size
+    return im.canvasSize
 
 @pyscript_executor
-def prepareImage(src, size, fitscreen=False):
+def prepareImage(src, canvasSize, namePrefix="", resize=False):
     
     _LOGGER = logging.getLogger("custom_components.pyscript.openhasp-pyscript")
 
-    imageID = hashlib.md5(src.encode("utf-8")).hexdigest()
+    imageID = f"{namePrefix}-{str(time.time()).replace(".","_")}"
 
     if src.startswith("http"):
         im = Image.open(requests.get(src, stream=True).raw)
@@ -30,18 +32,18 @@ def prepareImage(src, size, fitscreen=False):
         im = Image.open(src)
 
     original_width, original_height = im.size
-    width, height = size
+    width, height = canvasSize
 
-    if not fitscreen:
+    if not resize:
         width = min(w for w in [width, original_width] if w is not None and w > 0)
         height = min(h for h in [height, original_height] if h is not None and h > 0)
         im.thumbnail((height, width), Image.LANCZOS)
     else:
         im = im.resize((height, width), Image.LANCZOS)
-    width, height = im.size  # actual size after resize
+    width, height = im.size  # actual canvasSize after resize
 
     localFilename = f"/config/www/openhasp-pyscript/temp/{imageID}"
-    publicFileName = f"http://192.168.0.4:8123/local/openhasp-pyscript/temp/{imageID}"
+    publicFileName = f"{get_url(hass, allow_external=False)}/local/openhasp-pyscript/temp/{imageID}"
     with open(localFilename, "wb") as out_image:
         out_image.write(struct.pack("I", height << 21 | width << 10 | 4))    
         img = im.convert("RGB")
@@ -51,13 +53,6 @@ def prepareImage(src, size, fitscreen=False):
             b = (pix[2] >> 3) & 0x1F
             out_image.write(struct.pack("H", (r << 11) | (g << 5) | b))
     
-        _LOGGER.debug(
-            "image_to_rgb565 out_image: %s - %s > %s",
-            out_image.name,
-            (original_width, original_height),
-            im.size,
-        )
-    
         out_image.flush()
 
-    return publicFileName
+    return publicFileName, im.size
