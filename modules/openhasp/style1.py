b5d84d54820289a8530906f16e6f0cd9e3a7cdb8
Frederic Goddeeris <FredericGoddeeris@hotmail.com>
2024-04-01 11:02:15 +0200

    Renamed styles 3 and 4 to 1 and 2

--- modules/openhasp/style1.py
+++ modules/openhasp/style1.py
@@ -5,14 +5,14 @@ PAL_BEIGE = "#F0EDCF"
 
 style = (  {"page.gb_color":       PAL_BLACK,
             
-            "text.font":       30,
+            "text.font":           30,
             "text.color":          PAL_LIGHTBLUE,
             "text.align":          "center",
             "text.on.color":       "Gold",
             "text.off.color":      "Linen",
             
             "btn.text_color":      "Linen",
-            "btn.font":        30,
+            "btn.font":            30,
             "btn.bg_color":        PAL_BLACK,
             "btn.radius":          20,
             "btn.align":           "center",
@@ -20,6 +20,7 @@ style = (  {"page.gb_color":       PAL_BLACK,
             "btn.border_width":    2,
             "btn.on.bg_color":     PAL_DARKBLUE,
             "btn.off.bg_color":    PAL_BLACK,
+            "btn.icon.font":       50,
 
             "switch.border_color":  PAL_LIGHTBLUE,
             "switch.off.bg_color":  PAL_BLACK,
