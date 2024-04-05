88d9e0160b1aa1d00bef092d9f18725d7d8fc801
Frederic Goddeeris <FredericGoddeeris@hotmail.com>
2024-04-05 20:01:45 +0200

    Small corrections

--- apps/openhasp_helloWorld.py
+++ apps/openhasp_helloWorld.py
@@ -1,7 +1,7 @@
 import openhasp as oh
 import openhasp.style2 as myStyle # Or select another style
 
-# Adapt the setting below
+# Adapt the settings below
 PLATE_NAME = "plate_testing"
 PLATE_RESOLUTION = (480,320)
 MY_LAMP_ENTITY = "light.bureau_spots"
@@ -30,7 +30,7 @@ obj.linkText("sensor.time") # Link the label text to HA entity sensor.time
 oh.AnalogClock(design, center=(480/2,130), r=100, timeSource="sensor.time")
 
 # Add a on/off button controlling a light
-oh.OnOffButton(design, coord=(50,250), size=(380,60), text="My Lamp", entity=MY_LAMP_ENTITY, icon=oh.ICON_WALL_SCONCE)
+oh.OnOffButton(design, coord=(50,250), size=(380,60), text="Push me", entity=MY_LAMP_ENTITY, icon=oh.ICON_WALL_SCONCE)
 
 # Send the design now (and also later when the plate would restart)
 manager.sendDesign()
