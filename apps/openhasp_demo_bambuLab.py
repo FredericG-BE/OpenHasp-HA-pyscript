5ae5d557c1a8ba7d302f6de69c6e68cd4f391d0c
Frederic Goddeeris <FredericGoddeeris@hotmail.com>
2024-05-05 16:13:39 +0200

    PrintTime Arc

--- apps/openhasp_demo_bambuLab.py
+++ apps/openhasp_demo_bambuLab.py
@@ -1,7 +1,37 @@
 import openhasp as oh
 import openhasp.mdi as mdi
-from openhasp import Manager, ComposedObj
+from openhasp import Manager, ComposedObj, Obj
 from openhasp.style1 import style as myStyle
+
+import datetime
+from homeassistant.util.dt import as_local
+
+
+class Arc(Obj):
+    def __init__(self, design, coord, size, min=0, max=100, value=50, rotation=0, color=None, adjustable=False, startAngle=None, endAngle=None, startAngle10=None, endAngle10=None):
+        self.Obj__init__(design=design, type="arc", coord=coord, size=size)
+        
+        self.setParam("min", min)
+        self.setParam("max", max)
+        self.setParam("val", value)
+        self.setParam("rotation", rotation)
+        if adjustable is not None:
+            self.setParam("adjustable", adjustable)
+        if startAngle is not None:    
+            self.setParam("start_angle", startAngle)
+        if endAngle is not None:
+            self.setParam("endAngle", endAngle)
+        if startAngle10 is not None:
+            self.setParam("start_angle10", startAngle10)
+        if endAngle10 is not None:
+            self.setParam("end_angle10", endAngle10)
+
+        if color is not None:
+            self.setParam("line_color10", color)
+
+    def setValue(self, value):
+        self.setParam("val", value)
+        
 
 
 class BambuLabPrinter(ComposedObj):
@@ -9,69 +39,80 @@ class BambuLabPrinter(ComposedObj):
         self.ComposedObj__init__(design)
         self.printer = printer
 
-        self.camera = oh.Camera(design, (0,0), size, f"image.{printer}_camera")
+        self.camera = oh.Camera(design, coord, size, f"image.{printer}_camera")
 
-        infoWindowCoord = (coord[0]+15, coord[1]+15)
-        infoWindowSize = (size[0]-30, 150)
-        # obj = oh.EmptyObj(design, infoWindowCoord, infoWindowSize)
-        # obj.setParam("bg_opa", 60*255/100)
-        # obj.setParam("bg_color", None, "page.gb_color")
-        # obj.setBorder(self.design.style["btn.border_width"], self.design.style["btn.radius"], self.design.style["text.color"])
-
-        x = infoWindowCoord[0] + 15
-        y = infoWindowCoord[1]        
-
-        self.camera = oh.Camera(design, (x,y+10), (130, 130), f"image.{printer}_cover_image")
-        x += 150
+        clockR = 70
+        margin = 15
+        y = coord[1] + margin
+        
+        self.coverImage = oh.Camera(design, (coord[0]+15, y), (clockR*2, clockR*2), f"image.{printer}_cover_image", refreshRateSec=None)
 
         self.statusLabel = oh.Label(design, (coord[0]+size[0]/2-200, 10), (400, 50), "", align="center")
         self.statusLabel.setParam("bg_opa", 80*255/100)
         self.statusLabel.setParam("bg_color", None, "page.gb_color")
         self.statusLabel.setBorder(self.design.style["btn.border_width"], self.design.style["btn.radius"], self.design.style["text.color"])
-
-        self.tf1 = oh.triggerFactory_entityChange(f"sensor.{printer}_current_stage", self._onStateChange, self.design.manager.instanceId)
-        self.tf2 = oh.triggerFactory_entityChange(f"sensor.{printer}_print_progress", self._onStateChange, self.design.manager.instanceId)
-        self._onStateChange(self.design.manager.instanceId) # Mimic a change so that the correct value is filled in
-
-
-        #self.statusLabel.linkText(f"sensor.{printer}_current_stage")
-        x += 150
-        # obj = oh.Label(design, (x,y), (200,50), "Remaining:", align="left")
-        # obj.linkText(f"sensor.{printer}_remaining_time")
-        # x += 100
-        # obj = oh.Label(design, (x,y), (300,50), "Layer:", align="left")
-        # obj.linkText(f"sensor.{printer}_current_layer")
-        # x += 100
-        # obj = oh.Label(design, (x,y), (300,50), "End Time:", align="left")
-        # obj.linkText(f"sensor.{printer}_end_time")
-        r = infoWindowSize[1] / 2 * .8# obj = oh.EmptyObj(design, infoWindowCoord, infoWindowSize)
-        c = (infoWindowCoord[0]+infoWindowSize[0]-20-r,infoWindowCoord[1]+infoWindowSize[1]/2) 
-        bpr = r*1.1
+        
+        # Clock backplate
+        c = (coord[0] + size[0] - margin - clockR, y + clockR) 
+        
+        bpr = clockR
         obj = oh.EmptyObj(design, (c[0]-bpr, c[1]-bpr), (2*bpr,2*bpr))
         obj.setParam("bg_opa", 80*255/100)
         obj.setParam("bg_color", None, "page.gb_color")
         obj.setBorder(self.design.style["btn.border_width"], bpr, self.design.style["text.color"])
 
-        self.clock = oh.AnalogClock(design, c, r)
-        readyTime = oh.AnalogClock.Indicator(f"sensor.{printer}_end_time", timeFormat="%Y-%m-%d %H:%M:%S", color="Red") 
-        self.clock.addIndicator(readyTime)
+        # PrintTime Arc
+        arcR = clockR // 2
+        self.printTimeArc = Arc(design, (c[0]-arcR, c[1]-arcR), (2*arcR,2*arcR), rotation=-90, value=100, color="Red")
+        self.printTimeArc.setHidden(True)
+
+        # Clock
+        self.clock = oh.AnalogClock(design, c, clockR*0.95)
         
+        self.tf1 = oh.triggerFactory_entityChange(f"sensor.{printer}_current_stage", self._onStateChange, self.design.manager.instanceId)
+        self.tf2 = oh.triggerFactory_entityChange(f"sensor.{printer}_print_progress", self._onStateChange, self.design.manager.instanceId)
+        self.tf3 = oh.triggerFactory_entityChange(f"sensor.{printer}_start_time", self._onStateChange, self.design.manager.instanceId)
+        self.tf3 = oh.triggerFactory_entityChange(f"sensor.{printer}_end_time", self._onStateChange, self.design.manager.instanceId)
+        self._onStateChange(self.design.manager.instanceId) # Mimic a change so that the correct value is filled in        
 
     def _onStateChange(self, id):
+        def time2Angle(dts, timeFormat):
+            #try:
+            dt = datetime.datetime.strptime(dts, timeFormat)
+            dt = as_local(dt)
+            h = dt.hour
+            m = dt.minute
+            # except:
+            #     log.error(f"Failed to parse time '{dts}'")
+            #     h = 0
+            #     m = 0
+            if h >= 12: 
+                h -= 12
+            return int((h+m/60)/12*360)
+
         if not self.design.manager._checkInstanceId(id, "Printer StateChange"):
             return
-        stage = state.get(f"sensor.{self.printer}_current_stage")
-        # if stage == "auto_bed_leveling":
-        #     stage = "Auto Leveling"
-        # elif stage.find("preheating") >= 0:
-        #     stage = "Preheating"
-        # else:
-        stage = stage.replace("_", " ")
-        stage = stage[0].upper()+stage[1:]
-        progress = state.get(f"sensor.{self.printer}_print_progress")
-        self.statusLabel.setText(f"{stage} {progress}%")
         
+        stage = state.get(f"sensor.{self.printer}_current_stage") 
+        
+        # Set status label
+        status = stage.replace("_", " ")
+        status = status[0].upper()+status[1:]
+        if stage == "printing":
+            status += " " + state.get(f"sensor.{self.printer}_print_progress") + "%"
+        self.statusLabel.setText(status)
 
+        # Set start/end indicator
+        if stage != "idle":
+            startTimeAngle = time2Angle(state.get(f"sensor.{self.printer}_start_time"), timeFormat="%Y-%m-%d %H:%M:%S")
+            endTimeAngle = time2Angle(state.get(f"sensor.{self.printer}_end_time"), timeFormat="%Y-%m-%d %H:%M:%S")
+            self.printTimeArc.setParam("start_angle", startTimeAngle)
+            self.printTimeArc.setParam("end_angle", endTimeAngle)
+            self.printTimeArc.setHidden(False)
+        else:
+            self.printTimeArc.setHidden(True)
+        
+        self.coverImage.refresh()
 
 
 class MyPlateManager(Manager):
@@ -105,3 +146,4 @@ def main():
         manager = MyPlateManager(name, plateName, resolution, printer)
         managers.append(manager)
         manager.sendDesign()
+
